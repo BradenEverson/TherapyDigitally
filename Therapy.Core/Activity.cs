6301a9a554d4ef53315dfb8a9888e1d03a473759
Braden Everson <bradeneverson@gmail.com>
2020-09-12 11:00:26 -0500

    Added signalr, a user class, and the core class library. Made the base class for a therapy ticket and an activity

---
+++ Therapy.Core/Activity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Therapy.Core
+{
+    public class Activity
+    {
+        public string activityType { get; }
+        public string userId { get; }
+        public List<string> directions { get; }//Special Conditions: {wait:int} creates a timer for that long, {reflect:string} creates a textarea, {poll:[string array]} creates a poll
+        public string ticketId { get; }
+        public string id { get; }
+        public Activity(string activityType, string userId, string ticketId)
+        {
+            this.ticketId = ticketId;
+            this.activityType = activityType;
+            this.userId = userId;
+            this.id = Guid.NewGuid().ToString().Split('-')[0];
+            //Code for scraping 
+        }
+    }
+}
---
+++ Therapy.Core/Ticket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Therapy.Core
+{
+    public class Ticket
+    {
+        public Activity prescribedActivity { get; set; }
+        public string userId { get; set; }
+        public List<string> details { get; set; }
+        public DateTime prescriptionDate { get; }
+        public string diagnosis { get; set; }
+        public string id { get; }
+        public Ticket()
+        {
+            this.prescriptionDate = DateTime.Now;
+            this.id = Guid.NewGuid().ToString().Split('-')[0];
+        }
+        public void generateActivity()
+        {
+            Dictionary<string, int> everyDiagnosis = new Dictionary<string, int>();
+            foreach (string detail in details)
+            {
+                //String diagnosis = {Scrape specific detail from a health database to see which illness it most likely is}
+                //if(everyDiagnosis.containsKey(diagnosis){
+                    //everyDiagnosis[diagnosis] = everyDiagnosis[diagnosis] + 1;
+                //}else{
+                    //everyDiagnosis.add(diagnosis,1);
+                //}
+            }
+            //Cross reference every detail to see which illness is the most common in the list. Then set the diagnosis to that.
+            int max = everyDiagnosis.Values.Max();
+            this.diagnosis = everyDiagnosis.FirstOrDefault(r => r.Value == max).Key;
+            this.prescribedActivity = new Activity(this.diagnosis, this.userId, this.id);
+        }
+    }
+}
